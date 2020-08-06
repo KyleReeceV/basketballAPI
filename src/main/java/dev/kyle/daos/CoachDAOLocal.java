5ddd74b02652a837ab78e26bb7e851290f1c1b50
Kyle Van Sprewenburg <kvansprewenburg@radford.edu>
2020-08-06 15:23:04 -0400

    Actual init commit 2

--- src/main/java/dev/kyle/daos/CoachDAOLocal.java
+++ src/main/java/dev/kyle/daos/CoachDAOLocal.java
@@ -1,5 +1,14 @@
 package dev.kyle.daos;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import dev.kyle.entities.Coach;
+
 public class CoachDAOLocal {
+
+	
+	private Map<Integer,Coach> coach_table = new HashMap<Integer,Coach>();
+	private int count = 1;
 
 }
--- src/main/java/dev/kyle/daos/PlayerDAOLocal.java
+++ src/main/java/dev/kyle/daos/PlayerDAOLocal.java
@@ -1,5 +1,13 @@
 package dev.kyle.daos;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import dev.kyle.entities.Player;
+
 public class PlayerDAOLocal {
 
+	private Map<Integer,Player> player_table = new HashMap<Integer,Player>();
+	private int count = 1;
+	
 }
